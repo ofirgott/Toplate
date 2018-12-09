7c43cf3f53567f4eb99abdb97cc018f1ed0559aa
ofirgott <gottesmanofir@gmail.com>
2018-12-09 13:03:01 +0200

    test ofir

--- app/src/main/java/com/example/android/helloworld/DishActivity.java
+++ app/src/main/java/com/example/android/helloworld/DishActivity.java
@@ -14,7 +14,7 @@ public class DishActivity extends AppCompatActivity {
 
         ListView reviewsList = (ListView)findViewById(R.id.reviwesList);
 
-        final String[] values = new String[] { "SO FUCKING DELICIOUS", "This dish is shit, lots of Kusbara", "It's ok, but I wouldn't try it again. But if it's a cold day and you want something that warms you up so you wouldn't feel lonely, then it's ok. Overall it's ok, like really ok", "Yummm", "I wish I could marry Bok Choy <3", "Blackberry", "Blackberry", "Blackberry", "Blackberry", "Blackberry", "Blackberry", "Blackberry", "Blackberry"};
+        final String[] values = new String[] { "SO FUUCKING DELICIOUS", "This dish is shit, lots of Kusbara", "It's ok, but I wouldn't try it again. But if it's a cold day and you want something that warms you up so you wouldn't feel lonely, then it's ok. Overall it's ok, like really ok", "Yummm", "I wish I could marry Bok Choy <3", "Blackberry", "Blackberry", "Blackberry", "Blackberry", "Blackberry", "Blackberry", "Blackberry", "Blackberry"};
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
         reviewsList.setAdapter(adapter);
 
