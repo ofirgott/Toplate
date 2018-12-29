fb72301236764b10ddfcee9715505b4143d73f3c
Ofir Gottesman <Ofir.gottesman@genesys.com>
2018-12-29 18:05:57 +0200

    AUTH cont

--- app/src/main/java/com/example/android/helloworld/MainActivity.java
+++ app/src/main/java/com/example/android/helloworld/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -19,9 +21,9 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     List<AuthUI.IdpConfig> providers = Arrays.asList(
 
-            new AuthUI.IdpConfig.PhoneBuilder().build(),
-            new AuthUI.IdpConfig.GoogleBuilder().build(),
-            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.EmailBuilder().setRequireName(true).build(),
+            new AuthUI.IdpConfig.GoogleBuilder().setSignInOptions(GoogleSignInOptions.DEFAULT_SIGN_IN).build(),
+            new AuthUI.IdpConfig.PhoneBuilder().setDefaultCountryIso("il").build(),
             new AuthUI.IdpConfig.FacebookBuilder().build());
 
     @Override
@@ -37,23 +39,40 @@ public class MainActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    Intent a = new Intent(MainActivity.this, MenuActivity.class);
+                    Intent a = new Intent(MainActivity.this, MyProfile.class);
                     startActivity(a);
                      Toast.makeText(MainActivity.this, "User Signed In", Toast.LENGTH_SHORT).show();
                 } else {
                     startActivityForResult(
                             AuthUI.getInstance()
                                     .createSignInIntentBuilder()
+                                    .setAlwaysShowSignInMethodScreen(true)
                                     .setAvailableProviders(providers)
                                     .setIsSmartLockEnabled(false)
                                     .setLogo(R.mipmap.ic_launcher_foreground)
+
                                     .build(),
                             RC_SIGN_IN
                     );
-
                 }
+                Intent intent = new Intent(MainActivity.this, SearchFragment.class);
+                startActivity(intent);
             }
         };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RC_SIGN_IN) {
+            if(resultCode == RESULT_OK) {
+                //user logged in
+                Log.d("AUTH","Current user e-Mail: " + mFirebaseAuth.getCurrentUser().getEmail());
+                Log.d("AUTH","Current user Display Name: " + mFirebaseAuth.getCurrentUser().getDisplayName());
+                Intent intent = new Intent(this, SearchFragment.class);
+                startActivity(intent);
+            }
+        }
     }
 
     @Override
