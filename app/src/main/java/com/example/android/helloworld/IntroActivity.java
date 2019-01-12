50cb5c5409b734faad7bc7f0f64cd296d7b81c8a
Ofir Gottesman <Ofir.gottesman@genesys.com>
2019-01-12 16:39:27 +0200

    appIntro start design

--- app/src/main/java/com/example/android/helloworld/IntroActivity.java
+++ app/src/main/java/com/example/android/helloworld/IntroActivity.java
@@ -5,6 +5,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.github.paolorotolo.appintro.AppIntro;
@@ -20,25 +23,28 @@ public class IntroActivity extends AppIntro {
 
         // Add your slide fragments here.
         // AppIntro will automatically generate the dots indicator and buttons.
-        addSlide(new IntroSlide1());
-        addSlide(new IntroSlide2());
+//        addSlide(new IntroSlide1());
+//        addSlide(new IntroSlide2());
 
+        addSlide(new Intro1());
+        addSlide(new Intro2());
+        addSlide(new Intro3());
         //addSlide(thirdFragment);
         //addSlide(fourthFragment);
 
         // Instead of fragments, you can also use our default slide.
         // Just create a `SliderPage` and provide title, description, background and image.
         // AppIntro will do the rest.
-        SliderPage sliderPage = new SliderPage();
-        sliderPage.setTitle("Welcome to Toplate!");
-        sliderPage.setImageDrawable(R.mipmap.logo);
-        sliderPage.setBgColor(Color.parseColor("#2196F3"));
-        addSlide(AppIntroFragment.newInstance(sliderPage));
+//        SliderPage sliderPage = new SliderPage();
+//        sliderPage.setTitle("Welcome to Toplate!");
+//        sliderPage.setImageDrawable(R.mipmap.logo);
+//        sliderPage.setBgColor(Color.parseColor("#2196F3"));
+//        addSlide(AppIntroFragment.newInstance(sliderPage));
 
         // OPTIONAL METHODS
         // Override bar/separator color.
-        setBarColor(Color.parseColor("#3F51B5"));
-        setSeparatorColor(Color.parseColor("#2196F3"));
+        setBarColor(Color.parseColor("#000000"));
+        setSeparatorColor(Color.parseColor("#000000"));
 
         // Hide Skip/Done button.
         showSkipButton(true);
@@ -66,4 +72,34 @@ public class IntroActivity extends AppIntro {
 //        Intent a = new Intent(IntroActivity.this, MainActivity.class);
 //        startActivity(a);
     }
+
+    public static class Intro1 extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_intro1, container, false);
+        }
+    }
+
+    public static class Intro2 extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_intro2, container, false);
+        }
+
+    }
+
+    public static class Intro3 extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_intro3, container, false);
+        }
+    }
+
+
 }
